2144c20450f17573240342b38adc0eb3fb44f057
Satyajit Sahoo <satyajit.happy@gmail.com>
2015-05-23 17:51:07 +0530

    Don't check for file existent

--- makerepo.sh
+++ makerepo.sh
@@ -11,10 +11,10 @@ for rel in ${releases[@]}; do
 	mkdir -p $repodir/{SRPMS,noarch,i686,x86_64}
 
 	# Copy files to repo
-	[[ -f SRPMS/$rel/*.src.rpm ]] && cp -fu SRPMS/$rel/*.src.rpm $repodir/SRPMS/
-	[[ -f RPMS/$rel/*.noarch.rpm ]] && cp -fu RPMS/$rel/*.noarch.rpm $repodir/noarch/
-	[[ -f RPMS/$rel/*.i686.rpm ]] && cp -fu RPMS/$rel/*.i686.rpm $repodir/i686/
-	[[ -f RPMS/$rel/*.x86_64.rpm ]] && cp -fu RPMS/$rel/*.x86_64.rpm $repodir/x86_64/
+	cp -fu SRPMS/$rel/*.src.rpm $repodir/SRPMS/
+	cp -fu RPMS/$rel/*.noarch.rpm $repodir/noarch/
+	cp -fu RPMS/$rel/*.i686.rpm $repodir/i686/
+	cp -fu RPMS/$rel/*.x86_64.rpm $repodir/x86_64/
 
 	# Create repo data
 	createrepo $repodir
